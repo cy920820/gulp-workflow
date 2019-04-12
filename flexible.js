ef1f24cbf5908abfbe4de42dace56c023599613a
Cui-y <cuiyang673308817@gmail.com>
2019-04-12 20:56:47 +0800

    update flexible

--- flexible.js
+++ flexible.js
@@ -1,16 +1,21 @@
 // 移动端rem适配方案
 
-(function(win, doc) {
+(function(doc, win) {
   let docEl = doc.documentElement
   let resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize'
   function setRemUnit () {
     let vw = docEl.clientWidth
+    if (!vw) return
     docEl.style.fontSize = 100 * (vw / 750) + 'px'
+    if (vw > 750) {
+      docEl.style.fontSize = 50 + 'px'
+    }
   }
 
   setRemUnit()
+  if (!doc.addEventListener) return 
   win.addEventListener(resizeEvt, setRemUnit, false)
-  win.addEventListener('DOMContentLoaded', setRemUnit, false)
+  doc.addEventListener('DOMContentLoaded', setRemUnit, false)
 
-})(window, document)
+})(document, window)
     
